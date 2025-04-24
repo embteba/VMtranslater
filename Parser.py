5456b07f0e162189c403e8efc2ec73129dce5789
embteba <chach@chachayuzucoo.>
2025-04-24 22:26:56 +0900

    コンストラクタでのファイルオープン時の処理修正、command_typeを新規実装

--- Parser.py
+++ Parser.py
@@ -10,7 +10,10 @@ class Parser:
          
         # ファイルの読み込み
         with open(filename_or_filepath, 'r') as f:
-            self.lines = f.readlines()
+            # 空白行を消す
+            filterd_lines = [judged_whitespace_line for judged_whitespace_line in f if judged_whitespace_line.strip()]
+            # コメント削除
+            self.lines = [judged_commment_line for judged_commment_line in filterd_lines if not judged_commment_line.startswith("//")]
             
         
     def has_more_commands(self) -> bool:
@@ -26,7 +29,48 @@ class Parser:
     def advance(self) -> None:
         # 次の命令を取得
         self.current_command_index += 1
-        self.current_command = self.lines[self.current_command_index]
+        self.current_command = self.lines[self.current_command_index].strip()
+        
+    
+    
+    def command_type(self) -> str:
+        # 命令の種類を取得
+        if self.current_command.startswith("push"):
+            return "C_PUSH"
+        elif self.current_command.startswith("pop"):
+            return "C_POP"
+        elif self.current_command.startswith("add"):
+            return "C_ARITHMETIC"
+        elif self.current_command.startswith("sub"):
+            return "C_ARITHMETIC"
+        elif self.current_command.startswith("neg"):
+            return "C_ARITHMETIC"
+        elif self.current_command.startswith("eq"):
+            return "C_ARITHMETIC"
+        elif self.current_command.startswith("gt"):
+            return "C_ARITHMETIC"
+        elif self.current_command.startswith("lt"):
+            return "C_ARITHMETIC"
+        elif self.current_command.startswith("and"):
+            return "C_ARITHMETIC"
+        elif self.current_command.startswith("or"):
+            return "C_ARITHMETIC"
+        elif self.current_command.startswith("not"):
+            return "C_ARITHMETIC"
+        elif self.current_command.startswith("label"):
+            return "C_LABEL"
+        elif self.current_command.startswith("goto"):
+            return "C_GOTO"
+        elif self.current_command.startswith("if-goto"):
+            return "C_IF"
+        elif self.current_command.startswith("function"):
+            return "C_FUNCTION"
+        elif self.current_command.startswith("call"):
+            return "C_CALL"
+        elif self.current_command.startswith("return"): 
+            return "C_RETURN"
+        else:
+            raise ValueError(f"Unknown command: {self.current_command}")
         
 
 
